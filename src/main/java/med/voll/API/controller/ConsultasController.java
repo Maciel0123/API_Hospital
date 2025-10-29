7f3e8898628d09464d346fff180e2b364cebe21b
Maciel0123 <mrmmjs70@gmail.com>
2025-10-29 20:43:16 -0300

    Adicionando Swagger

--- src/main/java/med/voll/API/controller/ConsultasController.java
+++ src/main/java/med/voll/API/controller/ConsultasController.java
@@ -1,5 +1,6 @@
 package med.voll.API.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.API.domain.consulta.AgendaDeConsultas;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/consultas")
+@SecurityRequirement(name = "bearer-key")
 public class ConsultasController {
 
     @Autowired
--- src/main/java/med/voll/API/controller/PacienteController.java
+++ src/main/java/med/voll/API/controller/PacienteController.java
@@ -1,5 +1,6 @@
 package med.voll.API.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.API.domain.medico.MedicoRepository;
@@ -14,6 +15,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/pacientes")
+@SecurityRequirement(name = "bearer-key")
 public class PacienteController {
 
     @Autowired
